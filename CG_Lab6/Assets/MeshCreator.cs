e81296fc830915ab69e859e85f14beece6dd5da6
DenisSeledtsov <48148418+DenisSeledtsov@users.noreply.github.com>
2020-05-28 18:53:10 +0300

    Lab 6 added

---
+++ CG_Lab6/Assets/MeshCreator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCreator : MonoBehaviour
+{
+    Mesh customMesh;
+    void Start()
+    {
+        customMesh = new Mesh();
+
+        customMesh.Clear();
+        Vector3[] vertices = new Vector3[8]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 1, 0),
+
+            new Vector3(0, 1, 2),
+            new Vector3(1, 1, 2),
+
+            new Vector3(0, 0, 2),
+            new Vector3(1, 0, 2)
+        };
+        customMesh.vertices = vertices;
+
+        int[] tris = new int[30]
+        {
+            0, 2, 1,
+            2, 3, 1,
+
+            3, 2, 4,
+            4, 5, 3,
+
+            7, 1, 5,
+            1, 3, 5,
+
+            2, 0, 4,
+            6, 4, 0,
+
+            0, 1, 7,
+            0, 7, 6
+        };
+        customMesh.triangles = tris;
+        customMesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().sharedMesh = customMesh;
+    }
+}
---
+++ CG_Lab6/Assets/RotationController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RotationController : MonoBehaviour
+{
+
+    Vector3 mPrevPos = Vector3.zero;
+    Vector3 mPosDelta = Vector3.zero;
+
+    Vector3 startPos = Vector3.zero;
+
+    public Slider sliderUpDown;
+    public Slider sliderLeftRight;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            mPosDelta = Input.mousePosition - mPrevPos;
+            if(Vector3.Dot(transform.up, Vector3.up) >= 0)
+            {
+                transform.Rotate(transform.up, Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+            }
+            else
+            {
+                transform.Rotate(transform.up, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+            }
+
+            transform.Rotate(Camera.main.transform.right, Vector3.Dot(mPosDelta, Camera.main.transform.up), Space.World);
+        }
+
+        mPrevPos = Input.mousePosition;
+    }
+
+    public void ChangePosition()
+    {
+        transform.position = new Vector3(startPos.x + sliderLeftRight.value, startPos.y, startPos.z + sliderUpDown.value);
+    }
+}
